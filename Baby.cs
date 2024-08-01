7e54d21775178a8239279dd72c948ac3f7a80fac
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-01 22:42:10 +0300

    Add project files.

---
+++ Baby.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    public class Baby
+    {
+        private string name;
+        private string surName;
+        private DateTime birthDate;
+
+        public string Name { get { return name; }
+        
+        set { name = value; }
+        }
+
+        public string Surname { 
+            get { return surName; }
+            set { surName = value; } 
+
+        }
+
+        public DateTime BirthDate
+        {
+
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaa");
+            birthDate = DateTime.Now;
+            
+        }
+        public  Baby(string name,string surName)
+        {
+            Console.WriteLine("Ingaaaa");
+            this.Name = name;
+            this.Surname = surName;
+            this.birthDate=DateTime.Now;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Const;
+using System;
+using System.ComponentModel.Design;
+
+namespace Constructor
+{
+
+
+    class Constructor
+    {
+
+        static void Main(string[] args)
+        {
+            Baby baby1 = new Baby();
+            Console.WriteLine($"Baby's name:{baby1.Name} Baby's surname:{baby1.Surname} Baby's Birthdate: {baby1.BirthDate}");
+
+
+            Console.WriteLine("Please enter your babyS name");
+            string babyName= Console.ReadLine();
+            Console.WriteLine("Please enter your babyS surname");
+            string babySurName= Console.ReadLine();
+
+            Baby baby2 = new Baby(babyName, babySurName);
+           Console.WriteLine($"Baby's name:{baby2.Name} Baby's surname:{baby2.Surname} Baby's Birthdate: {baby2.BirthDate}");
+
+        }
+    }
+}
