05f03d2eb6075ab5304bfdf1d7d6d2f74e862d82
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-01 22:56:05 +0300

    input to the describe

--- Baby.cs
+++ Baby.cs
@@ -8,40 +8,41 @@ namespace Const
 {
     public class Baby
     {
-        private string name;
-        private string surName;
-        private DateTime birthDate;
+        private string name;// baby's name field
+        private string surName;// baby's surname field
+        private DateTime birthDate;// baby's birth date field
 
-        public string Name { get { return name; }
+        public string Name { get { return name; } //create to the Name Property
         
         set { name = value; }
         }
 
-        public string Surname { 
+        public string Surname { //create to the SurName Property
+        
             get { return surName; }
             set { surName = value; } 
 
         }
 
-        public DateTime BirthDate
+        public DateTime BirthDate //create to the Birth Date Property
         {
 
             get { return birthDate; }
             set { birthDate = value; }
         }
 
-        public Baby()
+        public Baby() // Cosntructor method any parametres for baby instance
         {
-            Console.WriteLine("Ingaaaa");
-            birthDate = DateTime.Now;
-            
+            Console.WriteLine("Ingaaaa");//result the message
+            BirthDate = DateTime.Now;//assign the datebirth property date at now
+
         }
-        public  Baby(string name,string surName)
+        public  Baby(string name,string surName)// Cosntructor method with name surname parametres for baby instance
         {
             Console.WriteLine("Ingaaaa");
-            this.Name = name;
-            this.Surname = surName;
-            this.birthDate=DateTime.Now;
+            this.Name = name;// assaign to the Name property variable
+            this.Surname = surName; //assign to the  sur anme Name property variable
+            this.BirthDate=DateTime.Now; //assign the datebirth property date at now
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace Constructor
 {
 
 
-    class Constructor
+    class Constructor// create to the main classs
     {
 
         static void Main(string[] args)
         {
-            Baby baby1 = new Baby();
-            Console.WriteLine($"Baby's name:{baby1.Name} Baby's surname:{baby1.Surname} Baby's Birthdate: {baby1.BirthDate}");
+            Baby baby1 = new Baby();// call the constroctor method any parametres
+            Console.WriteLine($"Baby's name:{baby1.Name} Baby's surname:{baby1.Surname} Baby's Birthdate: {baby1.BirthDate}");// write to the baby1 instance inforömation at the console
 
 
             Console.WriteLine("Please enter your babyS name");
@@ -20,8 +20,8 @@ namespace Constructor
             Console.WriteLine("Please enter your babyS surname");
             string babySurName= Console.ReadLine();
 
-            Baby baby2 = new Baby(babyName, babySurName);
-           Console.WriteLine($"Baby's name:{baby2.Name} Baby's surname:{baby2.Surname} Baby's Birthdate: {baby2.BirthDate}");
+            Baby baby2 = new Baby(babyName, babySurName);//call the constructor2 with the name and surname parameters
+           Console.WriteLine($"Baby's name:{baby2.Name} Baby's surname:{baby2.Surname} Baby's Birthdate: {baby2.BirthDate}");// write to the baby2 instance inforömation at the console
 
         }
     }
